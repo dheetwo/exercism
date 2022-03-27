4b45a262f48bd763a360655e79296ef1da5f49f4
Daniel Hsu <danielhsu001@gmail.com>
2022-03-27 13:36:16 -0400

    loop through RNA translate to protein

--- python/protein-translation/protein_translation.py
+++ python/protein-translation/protein_translation.py
@@ -41,4 +41,24 @@ STOP = {
 }
 
 def proteins(strand):
-
+    protein_list = []
+    while len(strand) > 0:
+        print(strand[:3])
+        if strand[:3] in STOP:
+            break
+        elif strand[:3] in METHIONINE:
+            protein_list.append("Methionine")
+        elif strand[:3] in PHENYLALANINE:
+            protein_list.append("Phenylalanine")
+        elif strand[:3] in LEUCINE:
+            protein_list.append("Leucine")
+        elif strand[:3] in SERINE:
+            protein_list.append("Serine")
+        elif strand[:3] in TYROSINE:
+            protein_list.append("Tyrosine")
+        elif strand[:3] in CYSTEINE:
+            protein_list.append("Cysteine")
+        elif strand[:3] in TRYPTOPHAN:
+            protein_list.append("Tryptophan")
+        strand = strand[3:]
+    return protein_list
