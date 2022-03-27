b91bcccad1526a6163d5e32120ccdc981bce163f
Daniel Hsu <danielhsu001@gmail.com>
2022-03-27 12:59:28 -0400

    protein dictionaries created

---
+++ python/protein-translation/protein_translation.py
@@ -0,0 +1,44 @@
+# Globals for proteins
+METHIONINE = {
+    "AUG"
+}
+
+PHENYLALANINE = {
+    "UUU",
+    "UUC"
+}
+
+LEUCINE = {
+    "UUA",
+    "UUG"
+}
+
+SERINE = {
+    "UCU",
+    "UCC",
+    "UCA",
+    "UCG"
+}
+
+TYROSINE = {
+    "UAU",
+    "UAC"
+}
+
+CYSTEINE = {
+    "UGU",
+    "UGC"
+}
+
+TRYPTOPHAN = {
+    "UGG"
+}
+
+STOP = {
+    "UAA",
+    "UAG",
+    "UGA"
+}
+
+def proteins(strand):
+
