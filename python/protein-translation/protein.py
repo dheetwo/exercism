fbcd1405f379e8bacb87be9b81b14be8e56c8dc9
Daniel Hsu <danielhsu001@gmail.com>
2022-03-27 14:54:37 -0400

    created directory file for protein changed proteins() to fit proteins.py

---
+++ python/protein-translation/protein.py
@@ -0,0 +1,51 @@
+# Globals for proteins
+METHIONINE = {
+    "AUG"
+}
+
+PHENYLALANINE = {
+    "UUU",
+    "UUC"
+}
+
+LEUCINE = {
+    "UUA",
+    "UUG"
+}
+
+SERINE = {
+    "UCU",
+    "UCC",
+    "UCA",
+    "UCG"
+}
+
+TYROSINE = {
+    "UAU",
+    "UAC"
+}
+
+CYSTEINE = {
+    "UGU",
+    "UGC"
+}
+
+TRYPTOPHAN = {
+    "UGG"
+}
+
+STOP = {
+    "UAA",
+    "UAG",
+    "UGA"
+}
+
+protein_list = [
+    (METHIONINE, "Methionine"),
+    (PHENYLALANINE, "Phenylalanine"),
+    (LEUCINE, "Leucine"),
+    (SERINE, "Serine"),
+    (TYROSINE, "Tyrosine"),
+    (CYSTEINE, "Cysteine"),
+    (TRYPTOPHAN, "Tryptophan"),
+]
--- python/protein-translation/protein_translation.py
+++ python/protein-translation/protein_translation.py
@@ -1,64 +1,13 @@
-# Globals for proteins
-METHIONINE = {
-    "AUG"
-}
+from protein import protein_list, STOP
 
-PHENYLALANINE = {
-    "UUU",
-    "UUC"
-}
-
-LEUCINE = {
-    "UUA",
-    "UUG"
-}
-
-SERINE = {
-    "UCU",
-    "UCC",
-    "UCA",
-    "UCG"
-}
-
-TYROSINE = {
-    "UAU",
-    "UAC"
-}
-
-CYSTEINE = {
-    "UGU",
-    "UGC"
-}
-
-TRYPTOPHAN = {
-    "UGG"
-}
-
-STOP = {
-    "UAA",
-    "UAG",
-    "UGA"
-}
 
 def proteins(strand):
-    protein_list = []
+    translation = []
     while len(strand) > 0:
-        print(strand[:3])
         if strand[:3] in STOP:
             break
-        elif strand[:3] in METHIONINE:
-            protein_list.append("Methionine")
-        elif strand[:3] in PHENYLALANINE:
-            protein_list.append("Phenylalanine")
-        elif strand[:3] in LEUCINE:
-            protein_list.append("Leucine")
-        elif strand[:3] in SERINE:
-            protein_list.append("Serine")
-        elif strand[:3] in TYROSINE:
-            protein_list.append("Tyrosine")
-        elif strand[:3] in CYSTEINE:
-            protein_list.append("Cysteine")
-        elif strand[:3] in TRYPTOPHAN:
-            protein_list.append("Tryptophan")
+        for protein in protein_list:
+            if protein[0].__contains__(strand[:3]):
+                translation.append(protein[1])
         strand = strand[3:]
-    return protein_list
+    return translation
