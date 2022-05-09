1045846106ebe048d91a44778aafbed4fb27133d
Daniel Hsu <danielhsu001@gmail.com>
2022-05-09 19:38:38 -0400

    first version of plants: function obtains students plants from inputting their name and outputting the name of their plants from the garden

--- python/pythagorean-triplet/pythagorean_triplet.py
+++ python/pythagorean-triplet/pythagorean_triplet.py
@@ -1,27 +1,14 @@
-import math
-
-
-# def triplets_with_sum(number):
-#     triplets = []
-#     c = number // 2
-#     while c > number // 3:
-#         remainder = number - c
-#         for i in range(int(remainder / 2), 1, -1):
-#             if i >= c or remainder - i >= c:
-#                 break
-#             if pow(i, 2) + pow(remainder - i, 2) == pow(c, 2):
-#                 triplets.append([i, remainder - i, c])
-#                 break
-#             print(i, remainder - i, c)
-#         c -= 1
-#     return triplets
-
 def triplets_with_sum(number):
-    for a in range(int(number / 3) + 1, 1, -1):
-        for b in range(a, 1, -1):
-            c = math.sqrt(a ** 2 + b ** 2)
-            # print(c, math.floor(c), math.ceil(c))
-            if math.floor(c) == math.ceil(c) and int(c) == number - a - b:
-                print(a, b, c)
+    triplets = []
+    for a in range(1, number):
+        denom = 2 * (number - a)
+        num = 2 * a ** 2 + number ** 2 - 2 * number * a
+        if denom > 0 and num % denom == 0:
+            c = num // denom
+            b = number - a - c
+            if b > a:
+                triplets.append((a, b, c))
+    return triplets
+
 
 print(triplets_with_sum(30000))
