a32cdcc7cc1ec8e656b71cf65c43861f9fbae71a
Daniel Hsu <danielhsu001@gmail.com>
2022-05-16 17:27:06 -0400

    Luhn number test function created

--- python/luhn/luhn.py
+++ python/luhn/luhn.py
@@ -1,6 +1,32 @@
 class Luhn:
+    card_string = ""
+
     def __init__(self, card_num):
-        pass
+        self.card_string = card_num
 
     def valid(self):
-        pass
+        card_list = []
+        for character in self.card_string:
+            if not character.isnumeric():
+                if character != " ":
+                    return False
+            else:
+                card_list.append(int(character))
+        if len(card_list) < 2:
+            return False
+        for index, number in enumerate(card_list):
+            if len(card_list) % 2 == 0:
+                if index % 2 == 0:
+                    if 0 <= number <= 4:
+                        card_list[index] = 2 * number
+                    else:
+                        card_list[index] = 2 * number - 9
+            else:
+                if index % 2 != 0:
+                    if 0 <= number <= 4:
+                        card_list[index] = 2 * number
+                    else:
+                        card_list[index] = 2 * number - 9
+        print(card_list)
+        return sum(card_list) % 10 == 0
+
